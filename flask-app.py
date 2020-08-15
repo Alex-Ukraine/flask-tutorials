2ff5decb69a996a7d7dc3b3f5cf1d87d735c2b73
Alex-Ukraine <win.city18@gmail.com>
2020-08-15 14:11:36 +0300

    03. Python Flask Tutorial Full-Featured Web App Part 3 - Forms and User Input

--- flask-app.py
+++ flask-app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '1563578335d309c8ec8bacf2d93a4e84'
 
 posts = [
     {
@@ -21,13 +23,34 @@ posts = [
 
 @app.route('/home')
 @app.route('/')
-def hello_world():
+def home():
     return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@gmail.com' and form.password.data == 'password':
+            flash('Вы вошли', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash("Вход не выполнен, проверьте параметры входа!!!", "danger")
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
